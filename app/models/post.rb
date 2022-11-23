3b097db12fc26c7cf32409ea29f69467dc6f09ab
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-11-23 21:17:25 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validate :checks_title
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+
+    
+
+    def checks_title
+        if !(["Won't Believe","Secret","Top [number]","Guess"].any?{|phrase|"#{title}".include?(phrase)})
+            errors.add(:title, "must contain the specified")
+    end
 end
+end
+
+
